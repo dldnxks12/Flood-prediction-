e0fba23fcd647bb65c7ab213b8c5ebb68ccbb759
Jongsoo Lee <jjongs@postech.ac.kr>
2023-05-19 14:45:58 +0900

    Update phase5_reconstruction.py

--- phase5_reconstruction.py
+++ phase5_reconstruction.py
@@ -13,7 +13,16 @@ from sklearn.model_selection import train_test_split, ShuffleSplit
 import matplotlib.pyplot as plt
 
 def paint(label, pred):
-    pass
+    fig, axs = plt.subplots(1, 2)
+    cmap = plt.cm.plasma
+    cmap.set_under(color='black')  # set the color out of the range as black
+    im1 = axs[0].imshow(label, vmin=0.000001, vmax=3, cmap=cmap)
+    im2 = axs[1].imshow(pred, vmin=0.000001, vmax=3, cmap=cmap)
+    axs[0].set_title('label')
+    axs[1].set_title('pred')
+    fig.colorbar(im1, ax=axs, orientation='vertical')
+    plt.axis('on')
+    plt.show()
 
 def return_river_data(folder=None, file_name=None, skiprows=6):
     path = os.path.join(folder, file_name)
@@ -32,22 +41,13 @@ def gen_valid_dataset(low = 0, high = 690, ext='tif'):
     for i in range(660, high): # test data set 660 ~ 690
 
         pic_path = 'Depth (C{}).DEM_E5186_3m_with_FM.'.format(i + 1) + ext
-        data      = return_river_data(folder_path, pic_path)
+        label      = return_river_data(folder_path, pic_path)
 
-        data_pred              = deepcopy(data)*0
-        data_pred[valid_index] = total_pred_dict.item().get(i-660)
-        data_pred[np.where(data_pred < 0)] = 0
+        pred              = deepcopy(label)*0
+        pred[valid_index] = total_pred_dict.item().get(i-660)
+        pred[np.where(pred < 0)] = 0
 
-        fig, axs = plt.subplots(1, 2)
-        cmap = plt.cm.plasma
-        cmap.set_under(color='black')
-        im1 = axs[0].imshow(data, vmin=0.000001, vmax=3, cmap=cmap)
-        im2 = axs[1].imshow(data_pred, vmin=0.000001, vmax=3, cmap=cmap)
-        axs[0].set_title('label')
-        axs[1].set_title('pred')
-        fig.colorbar(im1, ax=axs, orientation='horizontal')
-        plt.axis('on')
-        plt.show()
+        paint(label, pred)
 
 
 if __name__ == "__main__":
