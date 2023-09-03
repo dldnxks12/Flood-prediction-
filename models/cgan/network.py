2d19620284e74af5f978c96b05bac6bf00d40982
Jongsoo Lee <jjongs@postech.ac.kr>
2023-09-04 02:15:10 +0900

    Create network.py

---
+++ models/cgan/network.py
@@ -0,0 +1,72 @@
+import os
+import sys
+import torch
+import torch.nn as nn
+import torch.utils.data
+
+class Discriminator(nn.Module):
+    def __init__(self, input_dim, hidden_dim = 512):
+        super(Discriminator, self).__init__()
+
+        self.linear1    = nn.Linear(input_dim,  hidden_dim)
+        self.linear2    = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3    = nn.Linear(hidden_dim, hidden_dim)
+        self.linear4    = nn.Linear(hidden_dim, 1)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+        self.sigmoid    = nn.Sigmoid()
+
+    def forward(self, x): # input : (batch_size, 1 x 28 x 28) flattened image
+        x = self.leaky_relu(self.linear1(x))
+        x = self.leaky_relu(self.linear2(x))
+        x = self.leaky_relu(self.linear3(x))
+        x = self.sigmoid(self.linear4(x)) # 0 ~ 1 사이로 -> 확률로 표현
+        return x
+
+class Generator(nn.Module):
+    def __init__(self, noise_dim, out_img_size, hidden_dim=512):
+        super(Generator, self).__init__()
+        self.linear1    = nn.Linear(noise_dim,  hidden_dim)
+        self.linear2    = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3    = nn.Linear(hidden_dim, out_img_size)
+        self.relu       = nn.ReLU()
+        self.tanh       = nn.Tanh()
+
+    def forward(self, noise):
+        x = self.relu(self.linear1(noise))
+        x = self.relu(self.linear2(x))
+        x = self.tanh(self.linear3(x))
+
+        return x
+class Conditional_Discriminator(nn.Module):
+    def __init__(self, input_dim, condition_dim, hidden_dim = 512):
+        super(Conditional_Discriminator, self).__init__()
+
+        self.linear1    = nn.Linear(input_dim + condition_dim,  hidden_dim)
+        self.linear2    = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3    = nn.Linear(hidden_dim, hidden_dim)
+        self.linear4    = nn.Linear(hidden_dim, 1)
+        self.leaky_relu = nn.LeakyReLU(0.2)
+        self.sigmoid    = nn.Sigmoid()
+
+    def forward(self, x): # input : (batch_size, 1 x 28 x 28) flattened image
+        x = self.leaky_relu(self.linear1(x))
+        x = self.leaky_relu(self.linear2(x))
+        x = self.leaky_relu(self.linear3(x))
+        x = self.sigmoid(self.linear4(x)) # 0 ~ 1 사이로 -> 확률로 표현
+        return x
+
+class Conditional_Generator(nn.Module):
+    def __init__(self, noise_dim, condition_dim, out_img_size, hidden_dim=512):
+        super(Conditional_Generator, self).__init__()
+        self.linear1    = nn.Linear(noise_dim + condition_dim,  hidden_dim)
+        self.linear2    = nn.Linear(hidden_dim, hidden_dim)
+        self.linear3    = nn.Linear(hidden_dim, out_img_size)
+        self.relu       = nn.ReLU()
+        self.tanh       = nn.Tanh()
+
+    def forward(self, noise):
+        x = self.relu(self.linear1(noise))
+        x = self.relu(self.linear2(x))
+        x = self.tanh(self.linear3(x))
+
+        return x
